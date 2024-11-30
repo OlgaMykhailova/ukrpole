d3b88d08fcc0b84773a08dd90955d6464d93a8b7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-11-30 20:07:39 +0200

    added font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,17 @@ export default {
         montserrat: ["var(--font-montserrat)"],
         alegreya: ["var(--font-alegreya)"],
       },
+      fontSize: {
+        "12reg": ["12px", { lineHeight: "17px", fontWeight: "400" }],
+        "14reg": ["14px", { lineHeight: "20px", fontWeight: "400" }],
+        "16reg": ["16px", { lineHeight: "22px", fontWeight: "400" }],
+        "18reg": ["18px", { lineHeight: "25px", fontWeight: "400" }],
+        "18med": ["18px", { lineHeight: "25px", fontWeight: "500" }],
+        "24med": ["24px", { lineHeight: "32px", fontWeight: "500" }],
+        "32med": ["32px", { lineHeight: "45px", fontWeight: "500" }],
+        "40med": ["40px", { lineHeight: "56px", fontWeight: "500" }],
+        "64med": ["64px", { lineHeight: "80px", fontWeight: "500" }],
+      },
       colors: {
         black: "#080A07",
         white: "#FFFFFC",
