21a466e4eae0963f1ab9668f45651318449251a8
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-04 12:09:56 +0200

    added button component (#9)

---
+++ src/types/buttonProps.ts
@@ -0,0 +1,10 @@
+export interface ButtonProps {
+  children: string | React.JSX.Element;
+  ariaLabel?: string;
+  className?: string;
+  type?: "submit" | "button";
+  variant?: "primary" | "secondary";
+  disabled?: boolean;
+  isLoading?: boolean;
+  onClick?: () => void;
+}
