ff2706e17bf23d82895ecf102ec2f9bd5c70a492
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-06 21:04:35 +0200

    Update callback pop up (#13)
    * updated modal translation
    
    * updated unsuccessful notification bg color
    
    * updated backdrop color
    
    * updated form indents

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,7 @@ export default {
         yellowLight: "#FAF1D5",
         darkRed: "#FF7272",
         red: "#FFAFAF",
+        backdrop: "#698587",
       },
       objectPosition: {
         "22%-center": "22% center",
