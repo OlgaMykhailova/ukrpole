797ebb15ed0913332fbae8305c6a538e46751660
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-06 18:12:40 +0200

    updated go to catalog button

--- src/components/home/products/Products.tsx
+++ src/components/home/products/Products.tsx
@@ -1,14 +1,18 @@
 import Button from "@/components/shared/buttons/Button";
 import Section from "@/components/shared/section/Section";
 import React from "react";
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
+import { Link } from "@/i18n/routing";
 
 export default function Products() {
   const t = useTranslations("buttons");
+  const locale = useLocale();
 
   return (
     <Section className="mt-4 tab:mt-8 laptop:mt-4">
-      <Button className="mx-auto">{t("goToCatalog")}</Button>
+      <Link href="/products" locale={locale} className="outline-none">
+        <Button className="mx-auto">{t("goToCatalog")}</Button>
+      </Link>
     </Section>
   );
 }
