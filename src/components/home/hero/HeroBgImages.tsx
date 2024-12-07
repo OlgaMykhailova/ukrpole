1cb5f1b93133a6e5a6114a609b764e330043d478
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-07 21:24:22 +0200

    updated hero figure

--- src/components/home/hero/HeroBgImages.tsx
+++ src/components/home/hero/HeroBgImages.tsx
@@ -10,9 +10,9 @@ export default function HeroBgImages() {
         width={2048}
         height={1024}
         priority
-        className="absolute top-0 left-0 -z-20 w-full h-[calc(100%-1px)] object-cover object-22%-center tab:origin-center"
+        className="absolute top-0 left-0 -z-20 w-full h-full object-cover object-22%-center tab:origin-center"
       />
-      <div className="absolute top-0 left-0 -z-10 w-full h-[calc(100%-1px)] bg-heroHomeGradient"></div>
+      <div className="absolute top-0 left-0 -z-10 w-full h-full bg-heroHomeGradient"></div>
       <Image
         src="/images/backgrounds/heroHome/heroHomeFigure.svg"
         alt="curved figure"
