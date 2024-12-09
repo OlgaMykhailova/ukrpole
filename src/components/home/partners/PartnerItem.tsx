0c2294d17744741365398b78c2901e2736cf424f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-09 20:39:34 +0200

    added partner item

--- src/components/home/partners/PartnerItem.tsx
+++ src/components/home/partners/PartnerItem.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
-export default function PartnerItem() {
-  return <li>PartnerItem</li>;
+interface PartnerItemProps {
+  partner: string;
+}
+
+export default function PartnerItem({ partner }: PartnerItemProps) {
+  return (
+    <li className="w-[155px] h-[66px] tab:w-[218px] tab:h-[92px] laptop:w-[252px] laptop:h-[109px] px-3 tab:px-4 laptop:px-5 rounded-[16px] bg-beige">
+      <Image
+        src={`/images/contentImages/homePartners/${partner}.webp`}
+        alt={partner}
+        width={428}
+        height={194}
+      />
+    </li>
+  );
 }
--- src/components/home/partners/Partners.tsx
+++ src/components/home/partners/Partners.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { useTranslations } from "next-intl";
 import Section from "@/components/shared/section/Section";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
+import PartnersList from "./PartnersList";
 
 export default function Partners() {
   const t = useTranslations("homePage.partners");
   return (
     <Section className="container max-w-[1720px]">
       <SectionTitle>{t("title")}</SectionTitle>
+      <PartnersList />
     </Section>
   );
 }
