cb406838b9ff1d029aede362751653c150436d67
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-10 21:15:58 +0200

    updated footer bg images

--- src/components/home/cooperation/Cooperation.tsx
+++ src/components/home/cooperation/Cooperation.tsx
@@ -9,13 +9,13 @@ export default function Cooperation() {
   const t = useTranslations();
 
   return (
-    <section className="relative mt-[calc(0.146*100vw-4px)] pb-20 tab:pb-[100px] scroll-mt-[72px] laptop:scroll-mt-[160px]">
+    <section className="relative mt-[calc(0.146*100vw-3px)] pb-20 tab:pb-[100px] scroll-mt-[72px] laptop:scroll-mt-[160px]">
       <Image
         src="/images/backgrounds/homeCooperation/field.svg"
         alt="green field"
         width="1280"
         height="187"
-        className="absolute -top-[calc(0.146*100vw-4px)] w-full h-auto"
+        className="absolute -top-[calc(0.146*100vw-3px)] w-full h-auto"
       />
       <div className="bg-green">
         <div className="container py-6 tab:py-10 max-w-[1720px]">
--- src/components/shared/footer/FooterBgImages.tsx
+++ src/components/shared/footer/FooterBgImages.tsx
@@ -3,29 +3,28 @@ import Image from "next/image";
 
 export default function FooterBgImages() {
   return (
-    <div className="relative">
+    <>
       <Image
         src="/images/backgrounds/footer/bgMob.svg"
         alt="green background"
         width="360"
         height="73"
-        className="tab:hidden w-full h-auto"
+        className="absolute -top-[calc(0.203*100vw-3px)] tab:hidden w-full h-auto"
       />
       <Image
         src="/images/backgrounds/footer/bgTab.svg"
         alt="green background"
         width="768"
         height="98"
-        className="hidden tab:block laptop:hidden w-full h-auto"
+        className="absolute -top-[calc(0.128*100vw-3px)] hidden tab:block laptop:hidden w-full h-auto"
       />
       <Image
         src="/images/backgrounds/footer/bgDesk.svg"
         alt="green background"
         width="1280"
         height="108"
-        className="hidden laptop:block w-full h-auto"
+        className="absolute -top-[calc(0.084*100vw-3px)] hidden laptop:block w-full h-auto"
       />
-      <div className="absolute -bottom-1 left-0 -z-10 w-full h-[6px] bg-green"></div>
-    </div>
+    </>
   );
 }
