19296197cd1ecd5fdb89168dbd7c0c729abf6861
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-10 21:22:30 +0200

    added cooperation link

--- src/components/home/cooperation/Cooperation.tsx
+++ src/components/home/cooperation/Cooperation.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 import CooperationInfo from "./CooperationInfo";
 import Button from "@/components/shared/buttons/Button";
 import CooperationTitle from "./CooperationTitle";
+import { Link } from "@/i18n/routing";
 
 export default function Cooperation() {
   const t = useTranslations();
+  const locale = useLocale();
 
   return (
     <section className="relative mt-[calc(0.146*100vw-3px)] pb-20 tab:pb-[100px] scroll-mt-[72px] laptop:scroll-mt-[160px]">
@@ -22,9 +24,13 @@ export default function Cooperation() {
           <CooperationTitle />
           <div className="flex flex-col items-center justify-between h-full">
             <CooperationInfo />
-            <Button variant="secondary" className="mt-6 tab:mt-5">
-              {t("buttons.becomePartner")}
-            </Button>
+            <Link
+              href="/cooperation"
+              locale={locale}
+              className="mt-6 tab:mt-5 outline-none"
+            >
+              <Button variant="secondary">{t("buttons.becomePartner")}</Button>
+            </Link>
           </div>
         </div>
       </div>
