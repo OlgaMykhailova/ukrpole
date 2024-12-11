c2d7f91205026c04c65ec47cb6cf1bcb2cdd5d22
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-11 13:28:12 +0200

    added news section

---
+++ src/components/home/news/News.tsx
@@ -0,0 +1,26 @@
+import Section from "@/components/shared/section/Section";
+import React from "react";
+import { useTranslations, useLocale } from "next-intl";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import { Link } from "@/i18n/routing";
+import Button from "@/components/shared/buttons/Button";
+import NewsList from "./NewsList";
+
+export default function News() {
+  const t = useTranslations();
+  const locale = useLocale();
+
+  return (
+    <Section>
+      <SectionTitle>{t("homePage.news.title")}</SectionTitle>
+      <NewsList />
+      <Link
+        href="/news"
+        locale={locale}
+        className="group block mt-8 tab:mt-14 laptop:mt-16 outline-none"
+      >
+        <Button className="mx-auto">{t("buttons.seeAll")}</Button>
+      </Link>
+    </Section>
+  );
+}
---
+++ src/components/home/news/NewsItem.tsx
@@ -0,0 +1,44 @@
+import Subtitle from "@/components/shared/titles/Subtitle";
+import Image from "next/image";
+import React from "react";
+import { useTranslations, useLocale } from "next-intl";
+import { Link } from "@/i18n/routing";
+
+interface NewsItemProps {
+  newsItem: {
+    title: string;
+    description: string;
+    id: string;
+  };
+  className?: string;
+}
+
+export default function NewsItem({ newsItem, className = "" }: NewsItemProps) {
+  const t = useTranslations();
+  const locale = useLocale();
+
+  const { title, description, id } = newsItem;
+
+  return (
+    <li className={`w-full tab:w-[46.6%] laptop:w-[48.9%] ${className}`}>
+      <Image
+        src={`/images/contentImages/homeNews/${id}.webp`}
+        alt={id}
+        width={1056}
+        height={700}
+        className="mb-6"
+      />
+      <Subtitle>{title}</Subtitle>
+      <p className="mt-4 line-clamp-4 laptop:line-clamp-3">{description}</p>
+      <Link
+        href={`/news/${id}`}
+        locale={locale}
+        className="block mt-4 outline-none text-16med tab:text-18med text-greenDark laptop:hover:text-greenKhaki focus:text-greenKhaki 
+        active:text-greenKhaki transition duration-300 
+        ease-out"
+      >
+        {t("homePage.news.readMore")}
+      </Link>
+    </li>
+  );
+}
