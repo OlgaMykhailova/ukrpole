56900b52fc8fa3e02602008b32ef5f611e87f490
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-11 19:09:16 +0200

    updated breadcrumbs styles

--- src/components/shared/breadcrumbs/Breadcrumbs.tsx
+++ src/components/shared/breadcrumbs/Breadcrumbs.tsx
@@ -16,20 +16,20 @@ export default function Breadcrumbs({ crumbs }: BreadcrumbsProps) {
   return (
     <nav
       aria-label="breadcrumbs"
-      className="container flex items-center max-w-[1720px] pt-2 tab:pt-4 laptop:pt-6 pb-10 laptop:pb-16 text-18reg"
+      className="container flex items-center max-w-[1720px] pt-2 tab:pt-4 laptop:pt-6 pb-10 laptop:pb-16"
     >
       <ul className="flex items-center">
         {crumbs.map((crumb, index) => (
-          <li key={crumb.href} className="flex items-center">
+          <li
+            key={crumb.href}
+            className="flex items-center text-18reg text-darkGrey laptop:hover:text-greenDark focus:text-greenDark 
+          active:text-greenDark transition duration-300 ease-out"
+          >
             {index > 0 && <span className="inline-block mx-2 tab:mx-3">/</span>}
             {index === crumbs.length - 1 ? (
               <span className="text-18med text-greenDark">{crumb.label}</span>
             ) : (
-              <Link
-                href={crumb.href}
-                locale={locale}
-                className="laptop:hover:text-greenDark focus:text-greenDark active:text-greenDark transition duration-300 ease-out outline-none"
-              >
+              <Link href={crumb.href} locale={locale} className="outline-none">
                 {crumb.label}
               </Link>
             )}
