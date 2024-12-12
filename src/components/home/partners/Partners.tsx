b18101b7dc7b8102cee8ab28693f70cbb4209372
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-12 14:50:15 +0200

    added about page partners section

--- src/components/home/partners/Partners.tsx
+++ src/components/home/partners/Partners.tsx
@@ -2,33 +2,16 @@ import React from "react";
 import { useTranslations } from "next-intl";
 import Section from "@/components/shared/section/Section";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
-import PartnersList from "./PartnersList";
-import PartnersSlider from "./PartnersSlider";
-
-const partnersList = [
-  "fora",
-  "delvi",
-  "varus",
-  "kyshenya",
-  "megamarket",
-  "atb",
-  "tavrija",
-  "metro",
-  "ultramarket",
-  "novus",
-  "fozzy",
-  "delikat",
-];
+import PartnersInfo from "./PartnersInfo";
 
 export default function Partners() {
   const t = useTranslations("homePage.partners");
 
   return (
     <>
-      <Section className="container sm:flex flex-col items-center max-w-[1720px]">
+      <Section className="sm:flex flex-col items-center max-w-[1720px]">
         <SectionTitle>{t("title")}</SectionTitle>
-        <PartnersSlider partnersList={partnersList} />
-        <PartnersList partnersList={partnersList} />
+        <PartnersInfo />
       </Section>
     </>
   );
