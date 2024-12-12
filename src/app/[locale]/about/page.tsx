19d6b1d74fd67604f08ddbcd1dcc03538a50fcc7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-12 20:13:35 +0200

    added about page hero images

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,25 +1,17 @@
 import React from "react";
 import Header from "@/components/shared/header/Header";
 import Footer from "@/components/shared/footer/Footer";
-import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
-import { useTranslations } from "next-intl";
 import Mission from "@/components/about/mission/Mission";
 import Partners from "@/components/about/partners/Partners";
 import Advantages from "@/components/about/advantages/Advantages";
+import Hero from "@/components/about/hero/Hero";
 
 export default function AboutPage() {
-  const t = useTranslations();
-
-  const crumbs = [
-    { label: t("breadcrumbs.home"), href: "/" },
-    { label: t("breadcrumbs.about"), href: "/about" },
-  ];
-
   return (
     <>
       <Header color="white" />
       <main className="flex-1 pt-[72px] laptop:pt-[160px]">
-        <Breadcrumbs crumbs={crumbs} />
+        <Hero />
         <Mission />
         <Advantages />
         <Partners />
---
+++ src/components/about/hero/Hero.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
+import { useTranslations } from "next-intl";
+
+export default function Hero() {
+  const t = useTranslations();
+
+  const crumbs = [
+    { label: t("breadcrumbs.home"), href: "/" },
+    { label: t("breadcrumbs.about"), href: "/about" },
+  ];
+
+  return (
+    <section className="relative w-full aspect-[720/320] sm:aspect-[2560/800] overflow-hidden">
+      <Breadcrumbs crumbs={crumbs} />
+      <Image
+        src="/images/backgrounds/aboutHero/aboutHeroMob.webp"
+        alt="field with tomatoes"
+        width={720}
+        height={320}
+        className="sm:hidden absolute -z-10 top-0 left-0 w-full h-auto"
+      />
+      <Image
+        src="/images/backgrounds/aboutHero/aboutHero.webp"
+        alt="field with tomatoes"
+        width={2560}
+        height={800}
+        className="hidden sm:block absolute -z-10 top-0 left-0 w-full h-auto"
+      />
+    </section>
+  );
+}
