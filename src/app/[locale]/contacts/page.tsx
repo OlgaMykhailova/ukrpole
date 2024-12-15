e0fe8d0435ca981ffbf7f6454d3537e6a595bb3d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-15 10:45:09 +0200

    added contacts page breadcrumbs

--- src/app/[locale]/contacts/page.tsx
+++ src/app/[locale]/contacts/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { useTranslations } from "next-intl";
+import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
 
 export default function Contact() {
-  return <div>Contact page</div>;
+  const t = useTranslations();
+
+  const crumbs = [
+    { label: t("breadcrumbs.home"), href: "/" },
+    { label: t("breadcrumbs.contacts"), href: "/contacts" },
+  ];
+
+  return (
+    <>
+      <Breadcrumbs crumbs={crumbs} />
+    </>
+  );
 }
