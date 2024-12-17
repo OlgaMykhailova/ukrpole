ded9182a68a5bac509d909d05b54c2021b806d55
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-17 11:17:54 +0200

    updated input border styles

--- src/components/shared/forms/formElements/CustomizedInput.tsx
+++ src/components/shared/forms/formElements/CustomizedInput.tsx
@@ -25,7 +25,7 @@ const labelStyles = "relative flex flex-col gap-y-1 w-full text-14reg";
 const fieldStyles =
   "relative w-full h-full px-3 py-[14px] placeholder-darkGrey border rounded-[8px] outline-none text-16reg transition duration-300 ease-out";
 const fieldWrapperStyles =
-  "relative group before:content-[''] before:absolute before:top-[-2.5px] before:left-[-2px] before:rounded-[8px] before:w-[calc(100%+5px)] before:h-[calc(100%+5px)] before:blur-[2px] before:transition before:duration-300 before:ease-out ";
+  "relative before:content-[''] before:absolute before:top-[-2.5px] before:left-[-2px] before:rounded-[8px] before:w-[calc(100%+5px)] before:h-[calc(100%+5px)] before:blur-[2px] before:transition before:duration-300 before:ease-out ";
 const errorStyles = "absolute bottom-[-22px] right-0 text-12reg text-darkRed";
 
 export default function CustomizedInput({
@@ -73,7 +73,7 @@ export default function CustomizedInput({
           className={`${fieldStyles} ${fieldClassName} ${
             errors[fieldName] && touched[fieldName]
               ? "border-red"
-              : "border-grey focus:border-green"
+              : "border-grey focus-within:border-green"
           }`}
         ></Field>
       </div>
