e7ddc923d4c8f0df48c788f3e1175653f4923a45
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-17 20:33:46 +0200

    added not found page

--- src/components/notFound/NotFoundPage.tsx
+++ src/components/notFound/NotFoundPage.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import Section from "../shared/section/Section";
+import Image from "next/image";
+import { useTranslations, useLocale } from "next-intl";
+import Button from "../shared/buttons/Button";
+import Link from "next/link";
 
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const t = useTranslations("");
+  const locale = useLocale();
+
+  return (
+    <Section className="flex flex-col items-center mt-14 tab:mt-10 laptop:mt-2">
+      <Image
+        src="/images/contentImages/notFound/notFound.webp"
+        alt="404 image"
+        width={2272}
+        height={958}
+        className="w-full sm:w-[608px] lg:w-[960px] 2xl:w-[1180px] mb-4 tab:mb-0 laptop:mb-5"
+      />
+      <h1 className="w-[288px] tab:w-[604px] mb-6 tab:mb-8 font-alegreya text-24med tab:text-32med uppercase text-center">
+        {t("notFoundPage.title")}
+      </h1>
+      <p className="w-[328px] tab:w-[604px] mb-4 tab:mb-6 text-center">
+        {t("notFoundPage.description")}
+      </p>
+
+      <Link href="/" locale={locale} className="outline-none">
+        <Button>{t("buttons.homePage")}</Button>
+      </Link>
+    </Section>
+  );
 }
--- src/components/shared/titles/PageTitle.tsx
+++ src/components/shared/titles/PageTitle.tsx
@@ -7,7 +7,7 @@ interface SectionTitleProps {
 export default function PageTitle({ children, className }: SectionTitleProps) {
   return (
     <h1
-      className={`w-full mb-8 tab:mb-14 laptop:mb-16 font-alegreya text-32med tab:text-40med uppercase ${className}`}
+      className={`mb-8 tab:mb-14 laptop:mb-16 font-alegreya text-32med tab:text-40med uppercase ${className}`}
     >
       {children}
     </h1>
