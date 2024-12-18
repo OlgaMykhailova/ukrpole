d0c98afec2d3d8cc89add4f0c205d9fe8d482bca
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-18 22:37:10 +0200

    Policy page (#33)
    * added policy messages
    
    * added policy page

--- src/app/[locale]/policy/page.tsx
+++ src/app/[locale]/policy/page.tsx
@@ -1,5 +1,41 @@
+import { useTranslations } from "next-intl";
 import React from "react";
+import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
+import Section1 from "@/components/policy/Section1";
+import Section2 from "@/components/policy/Section2";
+import Section3 from "@/components/policy/Section3";
+import Section4 from "@/components/policy/Section4";
+import Section5 from "@/components/policy/Section5";
+import Section6 from "@/components/policy/Section6";
+import Section7 from "@/components/policy/Section7";
+import PageTitle from "@/components/shared/titles/PageTitle";
+import Section from "@/components/shared/section/Section";
 
 export default function Policy() {
-  return <div>Policy page</div>;
+  const t = useTranslations("");
+  const crumbs = [
+    { label: t("breadcrumbs.home"), href: "/" },
+    { label: t("breadcrumbs.policy"), href: "/policy" },
+  ];
+
+  return (
+    <>
+      <Breadcrumbs crumbs={crumbs} />
+      <Section>
+        <PageTitle>{t("policyPage.title")}</PageTitle>
+        <p className="laptop:w-[57.4%] mb-8 laptop:mb-12">
+          {t("policyPage.description")}
+        </p>
+        <ul className="flex flex-col gap-y-8 tab:gap-y-10">
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Section5 />
+          <Section6 />
+          <Section7 />
+        </ul>
+      </Section>
+    </>
+  );
 }
---
+++ src/components/policy/Section2.tsx
@@ -0,0 +1,20 @@
+import { useTranslations } from "next-intl";
+import React from "react";
+
+import Subtitle from "../shared/titles/Subtitle";
+
+export default function Section2() {
+  const t = useTranslations("policyPage.section2");
+
+  return (
+    <li className="flex flex-col gap-y-4 tab:gap-y-6 laptop:flex-row justify-between">
+      <Subtitle className="laptop:w-[25.5%]">{t("title")}</Subtitle>
+      <ul className="flex flex-col gap-y-4 tab:gap-y-6 laptop:w-[68.1%]">
+        <li>{t("part1")}</li>
+        <li>{t("part2")}</li>
+        <li>{t("part3")}</li>
+        <li>{t("part4")}</li>
+      </ul>
+    </li>
+  );
+}
