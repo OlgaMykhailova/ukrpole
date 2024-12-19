37e2fce2ab3426d0d72873001039a98f7b521750
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-19 16:34:13 +0200

    updated section title decoration

--- src/components/shared/titles/SectionTitle.tsx
+++ src/components/shared/titles/SectionTitle.tsx
@@ -3,44 +3,24 @@ import React from "react";
 interface SectionTitleProps {
   children: string;
   className?: string;
-  variant?: "decorBegin" | "decorEnd" | "noDecor";
 }
 
 export default function SectionTitle({
   children,
   className,
-  variant = "decorEnd",
 }: SectionTitleProps) {
-  const words = children.split(" ");
-  const lastWord = words.pop();
-  const restOfTitle = words.join(" ");
-
   return (
     <h2
-      className={`mb-8 tab:mb-14 laptop:mb-16 font-alegreya text-32med tab:text-40med uppercase text-center ${className}`}
+      className={`relative mb-8 tab:mb-14 laptop:mb-16 font-alegreya text-32med tab:text-40med uppercase text-center ${className}`}
     >
+      {children}
       <Image
-        src="/images/backgrounds/sectionTitle/leaf.webp"
+        src="/images/backgrounds/sectionTitle/leaf.svg"
         alt="leaf icon"
-        width={215}
-        height={210}
-        className={`w-[59px] tab:w-[107px] h-auto ml-2 transform scale-x-[-1] ${
-          variant === "decorBegin" ? "inline-block" : "hidden"
-        }`}
+        width={112}
+        height={15}
+        className={`absolute bottom-[-15px] left-[calc(50%-56px)] `}
       />
-      {restOfTitle}{" "}
-      <span className="inline-flex items-center whitespace-nowrap">
-        {lastWord}
-        <Image
-          src="/images/backgrounds/sectionTitle/leaf.webp"
-          alt="leaf icon"
-          width={215}
-          height={210}
-          className={`w-[59px] tab:w-[107px] h-auto ml-2 ${
-            variant === "decorEnd" ? "inline-block" : "hidden"
-          }`}
-        />
-      </span>
     </h2>
   );
 }
