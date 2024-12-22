6a0662f52b69022c01e12ca74256569d8b2fd71d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-22 21:31:15 +0200

    added news page breadcrumbs

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { useTranslations } from "next-intl";
+import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
 
 export default function News() {
-  return <div>News page</div>;
+  const t = useTranslations();
+
+  const crumbs = [
+    { label: t("breadcrumbs.home"), href: "/" },
+    { label: t("breadcrumbs.news"), href: "/news" },
+  ];
+
+  return (
+    <>
+      <Breadcrumbs crumbs={crumbs} />
+    </>
+  );
 }
