e6cce0623f2792b4766f4883dab401e3ffd82b64
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-12-27 17:20:01 +0200

    added breadcrumbs and bg images to the hero section

--- src/components/products/ProductsHero.tsx
+++ src/components/products/ProductsHero.tsx
@@ -1,5 +1,35 @@
+import Image from "next/image";
 import React from "react";
+import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
+import { useTranslations } from "next-intl";
 
 export default function ProductsHero() {
-  return <section>Products</section>;
+  const t = useTranslations();
+
+  const crumbs = [
+    { label: t("breadcrumbs.home"), href: "/" },
+    { label: t("breadcrumbs.products"), href: "/products" },
+  ];
+
+  return (
+    <section className="relative aspect-[360/160] sm:aspect-[770/202]">
+      <Breadcrumbs crumbs={crumbs} variant="light" />
+      <Image
+        src="/images/backgrounds/productsHero/productsHeroMob.webp"
+        alt="field with tomatoes"
+        width={720}
+        height={320}
+        priority
+        className="sm:hidden absolute -z-10 top-0 left-0 w-full h-auto"
+      />
+      <Image
+        src="/images/backgrounds/productsHero/productsHero.webp"
+        alt="field with tomatoes"
+        width={2560}
+        height={720}
+        priority
+        className="hidden sm:block absolute -z-10 top-0 left-0 w-full h-auto"
+      />
+    </section>
+  );
 }
