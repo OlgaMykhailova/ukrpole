39e5884b3321f2b0f54d6350884cb4cbaa3b198e
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-04 18:35:55 +0200

    added peppers bg images for the hero

--- src/components/products/hero/Hero.tsx
+++ src/components/products/hero/Hero.tsx
@@ -5,6 +5,7 @@ import HeroBgImages from "./HeroBgImages";
 import HeroDescription from "./HeroDescription";
 import Button from "@/components/shared/buttons/Button";
 import { Link } from "@/i18n/routing";
+import PeppersBgImages from "./PeppersBgImages";
 
 export default function Hero() {
   const t = useTranslations();
@@ -16,7 +17,7 @@ export default function Hero() {
   ];
 
   return (
-    <section className="flex flex-col items-center pb-20 tab:pb-[100px]">
+    <section className="relative flex flex-col items-center pb-20 tab:pb-[100px]">
       <div className="relative w-full aspect-[360/160] sm:aspect-[770/202]">
         <Breadcrumbs crumbs={crumbs} variant="light" />
         <HeroBgImages />
@@ -25,6 +26,7 @@ export default function Hero() {
       <Link href="/contacts" locale={locale} className="group outline-none">
         <Button>{t("buttons.startCooperation")}</Button>
       </Link>
+      <PeppersBgImages />
     </section>
   );
 }
