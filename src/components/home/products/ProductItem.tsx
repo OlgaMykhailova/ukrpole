9a6a64f5fd907d6ff73892c75c4761dea03a4400
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-04 20:43:59 +0200

    updated product item link

--- src/components/home/products/ProductItem.tsx
+++ src/components/home/products/ProductItem.tsx
@@ -1,16 +1,32 @@
+import { Link } from "@/i18n/routing";
 import { TabMenuItem } from "@/types/tabMenuItem";
 import Image from "next/image";
 import React from "react";
+import { useLocale } from "next-intl";
+import { useRouter } from "next/navigation";
 
 interface ProductItemProps {
   activeTabData: TabMenuItem;
 }
 
 export default function ProductItem({ activeTabData }: ProductItemProps) {
+  const locale = useLocale();
+  const router = useRouter();
   const { id, label, description } = activeTabData;
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    router.push(`/products?category=${id}&page=1#catalog`);
+  };
+
   return (
     <div>
-      <div className="overflow-clip">
+      <Link
+        href={`/products`}
+        onClick={handleClick}
+        locale={locale}
+        className="outline-none"
+      >
         <Image
           src={`/images/contentImages/homeProducts/${id}.webp`}
           alt={label}
@@ -18,7 +34,7 @@ export default function ProductItem({ activeTabData }: ProductItemProps) {
           height={740}
           className="w-full h-auto max-w-[878px] mx-auto laptop:hover:scale-105 transition duration-[2000ms] ease-out"
         />
-      </div>
+      </Link>
       <p className="w-full max-w-[380px] tab:max-w-[746px] h-[134px] tab:h-auto mx-auto mt-4 tab:mt-6 text-center">
         {description}
       </p>
