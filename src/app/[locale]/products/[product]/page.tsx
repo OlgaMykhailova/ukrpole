48e1916545f09a3a8ff6e49f4918595ae074dd36
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-01-05 21:39:27 +0200

    added breadcrumbs for product pages (#39)

--- src/app/[locale]/products/[product]/page.tsx
+++ src/app/[locale]/products/[product]/page.tsx
@@ -1,5 +1,47 @@
 import React from "react";
+import { useTranslations } from "next-intl";
+import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
+import { Locale } from "@/types/locale";
+import { productsList } from "@/mockedData/products";
+import NotFound from "../../not-found";
 
-export default function ProductPage() {
-  return <></>;
+export default function ProductPage({
+  params,
+}: {
+  params: {
+    product: string;
+    locale: Locale;
+  };
+}) {
+  const t = useTranslations();
+
+  const { product, locale } = params;
+
+  const currentProduct = productsList[locale]?.find(
+    ({ trademark, id, volume }) => `${trademark.id}-${id}-${volume}` === product
+  );
+
+  if (!currentProduct) {
+    return <NotFound />;
+  }
+
+  const { title, volume, trademark, id } = currentProduct;
+
+  const crumbs = [
+    { label: t("breadcrumbs.home"), href: "/" },
+    { label: t("breadcrumbs.products"), href: "/products" },
+    {
+      label:
+        `${title} ${volume} ${t("productsPage.ml")} ${t(
+          "productsPage.trademark"
+        )} ${trademark.title}` || "",
+      href: `/products/${trademark.id}-${id}-${volume}` || "",
+    },
+  ];
+
+  return (
+    <>
+      <Breadcrumbs crumbs={crumbs} />
+    </>
+  );
 }
