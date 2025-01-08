1f3d89922f2a3a137058f1c34c39f1a81e4587ec
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-08 17:34:37 +0200

    updated home page cooperation section bg image

--- src/components/shared/forms/formElements/CustomizedInput.tsx
+++ src/components/shared/forms/formElements/CustomizedInput.tsx
@@ -25,7 +25,7 @@ const labelStyles = "relative flex flex-col gap-y-1 w-full text-14reg";
 const fieldStyles =
   "relative w-full h-full px-3 py-[14px] placeholder-darkGrey border rounded-[8px] outline-none text-16reg transition duration-300 ease-out";
 const fieldWrapperStyles =
-  "relative before:content-[''] before:absolute before:top-[-2.5px] before:left-[-2px] before:rounded-[8px] before:w-[calc(100%+5px)] before:h-[calc(100%+5px)] before:blur-[2px] before:transition before:duration-300 before:ease-out ";
+  "relative before:content-[''] before:absolute before:top-[-2.5px] before:left-[-2px] before:rounded-[8px] before:w-[calc(100%+5px)] before:h-[calc(100%+5px)] before:blur-[2px] before:transition before:duration-300 before:ease-out before:will-change-transform";
 const errorStyles = "absolute bottom-[-22px] right-0 text-12reg text-darkRed";
 
 export default function CustomizedInput({
