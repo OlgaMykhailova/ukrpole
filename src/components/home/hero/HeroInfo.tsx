01d7b083ede07da07ce25cadb69c33111a144c3c
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-09 15:45:43 +0200

    updated hero title

--- src/components/home/hero/HeroInfo.tsx
+++ src/components/home/hero/HeroInfo.tsx
@@ -9,7 +9,10 @@ export default function HeroInfo() {
 
   return (
     <div className="container flex flex-col items-center laptop:justify-center h-full laptop:h-[85%] py-20 tab:py-[72px]">
-      <h1 className="max-w-[328px] sm:max-w-[528px] tab:max-w-[804px] laptop:max-w-[1280px] mb-8 sm:mb-12 tab:mb-8 font-alegreya text-32med tab:text-40med laptop:text-64med text-center text-white">
+      <h1 className="sm:hidden max-w-[328px] mb-8 font-alegreya text-32med text-center text-white">
+        {t("homePage.hero.titleMob").toUpperCase()}
+      </h1>
+      <h1 className="hidden sm:block max-w-[528px] tab:max-w-[804px] laptop:max-w-[1280px] mb-12 tab:mb-8 font-alegreya text-32med tab:text-40med laptop:text-64med text-center text-white">
         {t("homePage.hero.title").toUpperCase()}
       </h1>
       <h2 className="max-w-[328px] tab:max-w-[752px] mb-[26px] sm:mb-[46px] tab:mb-[34px] text-center text-white">
