87510b7a823d9cceef2b9f4b6223d524be950a38
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-09 16:18:44 +0200

    updated hero images

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -4,7 +4,7 @@ import HeroInfo from "./HeroInfo";
 export default function Hero() {
   return (
     <section
-      className="relative max-w-[2048px] h-[560px] tab:h-[500px] laptop:h-[calc(100dvh-160px)] mx-auto mb-4 tab:mb-8 laptop:mb-4 
+      className="relative h-[560px] tab:h-[500px] laptop:h-[calc(100dvh-160px)] mx-auto mb-4 tab:mb-8 laptop:mb-4 
     overflow-hidden"
     >
       <HeroBgImages />
