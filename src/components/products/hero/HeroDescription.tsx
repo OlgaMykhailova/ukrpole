6a8ffc44cd94b6bc14b5333a9a941f1c41c89b0c
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-09 12:07:02 +0200

    updated hero description styles

--- src/components/products/hero/HeroDescription.tsx
+++ src/components/products/hero/HeroDescription.tsx
@@ -5,7 +5,7 @@ export default function HeroDescription() {
   const t = useTranslations("productsPage");
 
   return (
-    <h1 className="max-w-[400px] tab:max-w-[655px] mt-8 tab:mt-10 laptop:mt-8 mb-6 text-center">
+    <h1 className="container max-w-[400px] tab:max-w-[655px] mt-8 tab:mt-10 laptop:mt-8 mb-6 text-center">
       {t("description")}
     </h1>
   );
