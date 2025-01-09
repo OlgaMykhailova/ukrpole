ed30357caa9726e82cc558dfb3cf37e9845aef25
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-09 19:17:09 +0200

    changed replace to push, added search params in locale switcher

--- src/components/shared/header/LocaleSwitcher.tsx
+++ src/components/shared/header/LocaleSwitcher.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { routing, usePathname, useRouter } from "@/i18n/routing";
+import { useSearchParams } from "next/navigation";
 import { Locale } from "@/types/locale";
 
 interface LocaleSwitcherProps {
@@ -15,12 +16,14 @@ export default function LocaleSwitcher({
   const locales = routing.locales;
   const pathName = usePathname();
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   const handleLocaleChange = (newLocale: Locale) => {
     const hash = window.location.hash;
-    const newPath = `${pathName}${hash}`;
 
-    router.replace(newPath, { locale: newLocale });
+    const newPath = `${pathName}${hash}?${searchParams.toString()}`;
+
+    router.push(newPath, { locale: newLocale });
   };
 
   return (
