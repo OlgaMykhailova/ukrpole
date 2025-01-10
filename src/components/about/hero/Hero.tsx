0901c06ac0726d61bae50d256595e9981f6f3004
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-10 18:50:06 +0200

    added animation for about page hero section

--- src/components/about/hero/Hero.tsx
+++ src/components/about/hero/Hero.tsx
@@ -1,10 +1,15 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
 import { useTranslations } from "next-intl";
+import { useOnScreen } from "@/hooks/useOnScreen";
+
+const SECTION_ID = "about-page-hero";
 
 export default function Hero() {
   const t = useTranslations();
+  const { isVisible } = useOnScreen(SECTION_ID);
 
   const crumbs = [
     { label: t("breadcrumbs.home"), href: "/" },
@@ -12,7 +17,10 @@ export default function Hero() {
   ];
 
   return (
-    <section className="relative w-full aspect-[720/320] sm:aspect-[2560/800] overflow-hidden">
+    <section
+      id={SECTION_ID}
+      className="relative w-full aspect-[720/320] sm:aspect-[2560/800]"
+    >
       <Breadcrumbs crumbs={crumbs} variant="light" />
       <Image
         src="/images/backgrounds/aboutHero/aboutHeroMob.webp"
@@ -20,7 +28,11 @@ export default function Hero() {
         width={720}
         height={320}
         priority
-        className="sm:hidden absolute -z-10 top-0 left-0 w-full h-auto"
+        className={`sm:hidden absolute -z-10 top-0 left-0 w-full h-auto ${
+          isVisible
+            ? "opacity-100 scale-100 tanslate-x-0 translate-y-0"
+            : " opacity-75 scale-110 translate-x-[20px] translate-y-[20px]"
+        } transition duration-[2000ms] ease-out`}
       />
       <Image
         src="/images/backgrounds/aboutHero/aboutHero.webp"
@@ -28,7 +40,9 @@ export default function Hero() {
         width={2560}
         height={800}
         priority
-        className="hidden sm:block absolute -z-10 top-0 left-0 w-full h-auto"
+        className={`hidden sm:block absolute -z-10 top-0 left-0 w-full h-auto ${
+          isVisible ? "scale-100" : "scale-[115%]"
+        } transition duration-[600ms] ease-out`}
       />
     </section>
   );
