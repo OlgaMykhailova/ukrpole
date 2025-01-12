fc65b080f7246777454b04ce13e97154cc1b8760
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-12 16:36:16 +0200

    added animation to the home page cooperation section images

--- src/components/home/cooperation/Cooperation.tsx
+++ src/components/home/cooperation/Cooperation.tsx
@@ -1,24 +1,23 @@
-import Image from "next/image";
 import React from "react";
 import { useTranslations, useLocale } from "next-intl";
 import CooperationInfo from "./CooperationInfo";
 import Button from "@/components/shared/buttons/Button";
 import CooperationTitle from "./CooperationTitle";
 import { Link } from "@/i18n/routing";
+import CooperationBgImages from "./CooperationBgImages";
+
+const SECTION_ID = "home-page-cooperation-section";
 
 export default function Cooperation() {
   const t = useTranslations();
   const locale = useLocale();
 
   return (
-    <section className="relative w-full pb-20 tab:pb-[100px] scroll-mt-[72px] laptop:scroll-mt-[160px]">
-      <Image
-        src="/images/backgrounds/homeCooperation/field.svg"
-        alt="green field"
-        width="1280"
-        height="187"
-        className="w-full h-auto"
-      />
+    <section
+      id={SECTION_ID}
+      className="relative w-full pb-20 tab:pb-[100px] scroll-mt-[72px] laptop:scroll-mt-[160px]"
+    >
+      <CooperationBgImages sectionId={SECTION_ID} />
       <div className="bg-green">
         <div className="container py-6 tab:py-10 max-w-[1920px]">
           <CooperationTitle />
