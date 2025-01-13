2372c34191fb2ec25c39e5cb4641ee8197a46b1a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-01-13 13:25:17 +0200

    Animation partners section (#70)
    * added animation to the partners section
    
    * updated animation styles

--- src/components/home/partners/PartnerItem.tsx
+++ src/components/home/partners/PartnerItem.tsx
@@ -3,11 +3,18 @@ import React from "react";
 
 interface PartnerItemProps {
   partner: string;
+  className?: string;
 }
 
-export default function PartnerItem({ partner }: PartnerItemProps) {
+export default function PartnerItem({
+  partner,
+  className = "",
+}: PartnerItemProps) {
   return (
-    <li className="flex items-center justify-center w-[47%] sm:w-[30.9%] lg:w-[23%] laptop:w-[23.3%] h-auto aspect-[155/66] px-3 tab:px-4 laptop:px-5 rounded-[16px] bg-beige">
+    <li
+      className={`flex items-center justify-center w-[47%] sm:w-[22.8%] lg:w-[23%] laptop:w-[23.3%] h-auto aspect-[155/66] px-3 tab:px-4 
+    laptop:px-5 rounded-[16px] bg-beige transition duration-[1200ms] ease-out ${className}`}
+    >
       <Image
         src={`/images/contentImages/homePartners/${partner}.webp`}
         alt={partner}
--- src/components/home/partners/PartnersList.tsx
+++ src/components/home/partners/PartnersList.tsx
@@ -1,15 +1,46 @@
+"use client";
 import React from "react";
 import PartnerItem from "./PartnerItem";
+import { useOnScreen } from "@/hooks/useOnScreen";
 
 interface PartnersListProps {
   partnersList: string[];
 }
 
+const SECTION_ID = "partners-section";
+
 export default function PartnersList({ partnersList }: PartnersListProps) {
+  const { isVisible } = useOnScreen(SECTION_ID, { threshold: 0.8 });
+
   return (
-    <ul className="hidden sm:flex flex-wrap justify-between sm:gap-4 tab:gap-x-6 tab:gap-y-8 w-full">
+    <ul
+      id={SECTION_ID}
+      className="hidden sm:flex flex-wrap justify-between sm:gap-4 lg:gap-x-6 lg:gap-y-8 w-full"
+    >
       {partnersList.map((partner, idx) => (
-        <PartnerItem key={idx} partner={partner} />
+        <PartnerItem
+          key={idx}
+          partner={partner}
+          className={`${
+            isVisible
+              ? "translate-x-0 opacity-100"
+              : `${
+                  idx === 0 || idx === 1
+                    ? "sm:-translate-x-[53.2%] lg:-translate-x-[39.7%] opacity-20"
+                    : idx === 2 || idx === 3
+                    ? "sm:translate-x-[53.2%] lg:translate-x-[39.7%] opacity-20"
+                    : idx === 4 || idx === 5
+                    ? "sm:-translate-x-[138.4%] lg:-translate-x-[103%] opacity-5"
+                    : idx === 6 || idx === 7
+                    ? "sm:translate-x-[138.4%] lg:translate-x-[103%] opacity-5"
+                    : idx === 8 || idx === 9
+                    ? "sm:-translate-x-[170.33%] lg:-translate-x-[127%] opacity-0"
+                    : idx === 10 || idx === 11
+                    ? "sm:translate-x-[170.33%] lg:translate-x-[127%] opacity-0"
+                    : ""
+                }`
+          }`}
+        />
       ))}
     </ul>
   );
