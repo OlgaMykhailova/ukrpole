23d2015183861c47b627239131b861d9dd53b56a
Olga Mykhailova <rosebud_z@yahoo.com>
2025-01-20 12:51:38 +0200

    updated logo ukrpole in the footer

--- src/components/shared/footer/FooterNavigation.tsx
+++ src/components/shared/footer/FooterNavigation.tsx
@@ -1,7 +1,7 @@
+import Image from "next/image";
 import React from "react";
 
 import IconLogoBataljon from "../icons/IconLogoBataljon";
-import LogoLink from "../logoLink/LogoLink";
 import NavMenu from "./navMenu/NavMenu";
 
 export default function FooterNavigation() {
@@ -9,7 +9,13 @@ export default function FooterNavigation() {
     <div className="bg-green">
       <div className="container flex flex-col items-center gap-6 laptop:flex-row laptop:justify-between max-w-[1920px] py-6 laptop:py-[62px]">
         <div className="flex flex-col tab:flex-row items-center gap-6 laptop:gap-7">
-          <LogoLink className="w-[182px] laptop:w-[167px] h-auto" />
+          <Image
+            src="/images/icons/logo.svg"
+            alt="logo"
+            width="204"
+            height="53"
+            className="w-[182px] laptop:w-[167px] h-auto"
+          />
           <IconLogoBataljon className="w-[129px] laptop:w-[138px] h-auto text-greenDark" />
         </div>
         <NavMenu />
