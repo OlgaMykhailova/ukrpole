d9be3e5da2840ccddc59acccafa7ee398bd23404
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-01-24 20:31:50 +0200

    Meta data article pages (#85)
    * added meta data for the article pages
    
    * updated article texts

--- src/app/[locale]/news/[article]/page.tsx
+++ src/app/[locale]/news/[article]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useTranslations } from "next-intl";
 import React from "react";
 
@@ -6,11 +5,22 @@ import Article from "@/components/article/Article";
 import Breadcrumbs from "@/components/shared/breadcrumbs/Breadcrumbs";
 import { articlesList } from "@/mockedData/articles";
 import { Locale } from "@/types/locale";
+import { generatePageMetaData } from "@/utils/generatePageMetaData";
 
 import NotFound from "../../not-found";
 
 interface ArticlePageProps {
   params: { article: string; locale: Locale };
+}
+
+export async function generateMetadata({ params }: ArticlePageProps) {
+  const { locale, article } = params;
+
+  return generatePageMetaData({
+    locale,
+    namespace: "metadata",
+    canonical: `/news/${article}`,
+  });
 }
 
 export default function ArticlePage({ params }: ArticlePageProps) {
